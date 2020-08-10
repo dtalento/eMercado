fbb3836ff24f64f12756d3c78c5e26bc06d7eb5b
Talento <68577325+dtalento@users.noreply.github.com>
2020-08-10 01:09:40 -0300

    Agregada la pagina de productos

--- js/products.js
+++ js/products.js
@@ -1,6 +1,39 @@
+function showProductList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <span class="price-tag">` +  product.currency + ` ` + product.cost + `</span>
+                    </div>
+                    <p>` + product.description + `</p>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("product-list").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(result){
+        if(result.status === "ok"){
+            productArray = result.data;
+            showProductList(productArray);
+        }
+    });
 
 });
