dddb3699c24b6569c57420153de5e1fdab7132c1
dtalento <68577325+dtalento@users.noreply.github.com>
2020-08-12 01:58:41 -0300

    Cambio en la funcion que muestra los productos utilizando nodos

--- js/products.js
+++ js/products.js
@@ -1,4 +1,4 @@
-function showProductList(array){
+function showProductListOld(array){
 
     let htmlContentToAppend = "";
     for(let i = 0; i < array.length; i++){
@@ -25,6 +25,53 @@ function showProductList(array){
     }
 }
 
+var showProductList = function(array){
+    container = document.getElementById("product-list"); // en este elemento se muestra la lista
+
+    for (let i = 0; i < array.length; i++){
+        let product = array[i];
+        
+        let productDiv = document.createElement("div");
+        productDiv.className = "list-group-item list-group-item-action" ;
+        container.appendChild(productDiv);
+        
+        let rowDiv = document.createElement("div");
+        rowDiv.className = "row" ;
+        productDiv.appendChild(rowDiv);
+
+        let imgDiv = document.createElement("div");
+        imgDiv.className = "col-3" ;
+        rowDiv.appendChild(imgDiv);
+        
+        let img = document.createElement("img");
+        img.src = product.imgSrc ;
+        img.alt = product.description ;
+        img.className = "img-thumbnail" ;
+        imgDiv.appendChild(img);
+
+        let dataDiv = document.createElement("div");
+        dataDiv.className = "col" ;
+        rowDiv.appendChild(dataDiv);
+
+        let dataHeaderDiv = document.createElement("div");
+        dataHeaderDiv.className = "d-flex w-100 justify-content-between" ;
+        dataDiv.appendChild(dataHeaderDiv);
+
+        let productName = document.createElement("h4");
+        productName.className = "mb-1" ;
+        productName.innerHTML = product.name ;
+        dataHeaderDiv.appendChild(productName);
+
+        let priceTag = document.createElement("span");
+        priceTag.innerHTML = product.currency + " " + product.cost ;
+        dataHeaderDiv.appendChild(priceTag);
+        
+        let description = document.createElement("p");
+        description.innerHTML = product.description ;
+        dataDiv.appendChild(description);
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -35,5 +82,4 @@ document.addEventListener("DOMContentLoaded", function (e) {
             showProductList(productArray);
         }
     });
-
 });
