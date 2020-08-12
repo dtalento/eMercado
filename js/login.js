cc74fa8757e8b225b642a7776ba5e15289055c6c
dtalento <68577325+dtalento@users.noreply.github.com>
2020-08-12 00:46:37 -0300

    Cambio el eventListener del login, del HTML al script

--- js/login.js
+++ js/login.js
@@ -1,21 +1,29 @@
-var logInDone = function(){
-    //Luego del log in volver a la página pedida antes de la redirección
-    // si esta no era la actual, sino volver al index
-     sessionStorage.setItem("loginFlag", "true");
-     if(sessionStorage.getItem("preLogInPage") == window.location.href) 
-     {
-       window.location.href = "index.html";
-     } else {
-       window.location.href = sessionStorage.getItem("preLogInPage");  
-     }
-   }
+var logInDone = function(event){
+  //Luego del log in volver a la página pedida antes de la redirección
+  // si esta no era la actual, sino volver al index
+  sessionStorage.setItem("loginFlag", "true");
+  if (sessionStorage.getItem("preLogInPage") == window.location.href) 
+  {
+    window.location.href = "index.html" ;
+  } else {
+    window.location.href = sessionStorage.getItem("preLogInPage");  
+  }
+}
 
 var logInRedirect = function(){
-    //Si volvemos al login luego de estar loggeados ir al index
-    if( window.location.href.endsWith("login.html") &&
-     sessionStorage.getItem("loginFlag") == "true"){
-        window.location.href = "index.html"
+  //Si volvemos al login luego de estar loggeados ir al index
+  if (window.location.href.endsWith("login.html") &&
+      sessionStorage.getItem("loginFlag") == "true"){
+      window.location.href = "index.html" ;
     }
+}
+
+var logInVerification = function(){
+  loginForm = document.getElementById("login-form");
+  loginForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    logInDone();
+  })
 }
 
 logInRedirect();
@@ -24,5 +32,5 @@ logInRedirect();
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+  logInVerification();
 });
