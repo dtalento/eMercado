7f1304ea77fb380d07dc50f36b6e4cfd8997e757
dtalento <68577325+dtalento@users.noreply.github.com>
2020-10-28 19:30:56 -0300

    Cambia estructura del html (perfil)

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,5 @@
+const defaultImageUrl = "https://i.ibb.co/fvnTgsR/mrmeow.jpg";
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
